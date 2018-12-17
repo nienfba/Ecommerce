28f28e182f86a999f64f13d21375e302072c6cf6
Fabien <fab@alti-com.fr>
2018-12-17 01:41:15 +0100

    Front and SQL upadate !

--- sql/cupoftea.sql
+++ sql/cupoftea.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  ven. 14 déc. 2018 à 14:28
+-- Généré le :  lun. 17 déc. 2018 à 00:40
 -- Version du serveur :  5.7.19
 -- Version de PHP :  7.1.9
 
@@ -35,7 +35,18 @@ CREATE TABLE IF NOT EXISTS `category` (
   `cat_description` text,
   `cat_picture` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`cat_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `category`
+--
+
+INSERT INTO `category` (`cat_id`, `cat_name`, `cat_description`, `cat_picture`) VALUES
+(1, 'Thé noir', '<p>Le th&eacute; noir, que les chinois appellent th&eacute; rouge en r&eacute;f&eacute;rence &agrave; la couleur cuivr&eacute;e de son infusion, est un th&eacute; compl&egrave;tement oxyd&eacute;. La fabrication du th&eacute; noir se fait en cinq &eacute;tapes : le fl&eacute;trissage, le roulage, l\'oxydation, la torr&eacute;faction et le triage. Cette derni&egrave;re op&eacute;ration permet de diff&eacute;rencier les diff&eacute;rents grades.</p>', '1.jpg'),
+(2, 'Thé vert', 'Réputé pour ses nombreuses vertus grâce à sa richesse en antioxydants, le thé vert désaltère, tonifie, apaise, fortifie, et procure une incontestable sensation de bien-être. Délicat et peu amer, il est apprécié à tout moment de la journée et propose une palette d\'arômes très variés : végétal, minéral, floral, fruité.', '2.jpg'),
+(3, 'Oolong', 'Les Oolong, que les chinois appellent thés bleu-vert en référence à la couleur de leurs feuilles infusées, sont des thés semi-oxydés : leur oxydation n\'a pas été menée à son terme. Spécialités de Chine et de Taïwan, il en existe une grande variété, en fonction de la région de culture, de l\'espèce du théier ou encore du processus de fabrication.', '3.jpg'),
+(4, 'Thé blanc', 'Le thé blanc est une spécialité de la province chinoise du Fujian. De toutes les familles de thé, c\'est celle dont la feuille est la moins transformée par rapport à son état naturel. Non oxydé, le thé blanc ne subit que deux opérations : un flétrissage et une dessiccation. Il existe deux grands types de thés blancs : les Aiguilles d\'Argent et les Bai Mu Dan.', '4.jpg'),
+(5, 'Rooibos', 'Le Rooibos (appelé thé rouge bien qu\'il ne s\'agisse pas de thé) est une plante poussant uniquement en Afrique du Sud et qui ne contient pas du tout de théine. Son infusion donne une boisson très agréable, ronde et légèrement sucrée. Riche en antioxydants, faible en tanins et dénué de théine, le Rooibos peut être dégusté en journée comme en soirée.', '5.jpg');
 
 -- --------------------------------------------------------
 
@@ -58,7 +69,14 @@ CREATE TABLE IF NOT EXISTS `customer` (
   `cust_createdDate` datetime DEFAULT NULL,
   `cust_birthday` date DEFAULT NULL,
   PRIMARY KEY (`cust_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `customer`
+--
+
+INSERT INTO `customer` (`cust_id`, `cust_firstname`, `cust_lastname`, `cust_email`, `cust_password`, `cust_address`, `cust_cp`, `cust_city`, `cust_country`, `cust_phone`, `cust_createdDate`, `cust_birthday`) VALUES
+(1, 'Fabien', 'Sellès', 'fabien.selles@alti-com.fr', '$2y$10$WyOWMI./qj6AwuYXulVNa.1BtmFFqQA8bGfr6dgRBuSGNToqs78ri', '59 Rue Carnot', '05000', 'GAP', 'France', '0608371743', '2018-12-16 01:03:52', '2018-12-13');
 
 -- --------------------------------------------------------
 
@@ -110,12 +128,21 @@ CREATE TABLE IF NOT EXISTS `product` (
   `prod_subtitle` varchar(255) DEFAULT NULL,
   `prod_description` text,
   `prod_createdDate` date DEFAULT NULL,
-  `prod_price` double DEFAULT NULL,
+  `prod_price` double UNSIGNED DEFAULT NULL,
+  `prod_tva` float UNSIGNED NOT NULL DEFAULT '20',
   `prod_picture` varchar(255) DEFAULT NULL,
   `category_cat_id` int(10) UNSIGNED NOT NULL,
   PRIMARY KEY (`prod_id`),
   KEY `fk_product_category1_idx` (`category_cat_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `product`
+--
+
+INSERT INTO `product` (`prod_id`, `prod_name`, `prod_subtitle`, `prod_description`, `prod_createdDate`, `prod_price`, `prod_tva`, `prod_picture`, `category_cat_id`) VALUES
+(1, 'Blue of London', 'Thé noir à la bergamote', '<section id=\"product-description\">\r\n<p>Blue of London est un Earl Grey d\'exception qui associe un des meilleurs th&eacute;s noirs au monde, le Yunnan, et une bergamote fra&icirc;che et d&eacute;licate. Un m&eacute;lange remarquable d\'&eacute;quilibre et de finesse.</p>\r\n<p>Le Earl Grey est un grand classique anglais, depuis que Charles Grey, comte (earl en anglais) de Falodon et Ministre des Affaires &eacute;trang&egrave;res du Royaume britannique au milieu du XIX &egrave;me si&egrave;cle, re&ccedil;ut d\'un mandarin chinois une vieille recette consistant &agrave; aromatiser son th&eacute; avec de la bergamote.</p>\r\n<p><strong>Profitez d\'une remise de 5% sur la pochette de 500g (prix d&eacute;j&agrave; remis&eacute;).</strong></p>\r\n<p><strong>Profitez d\'une remise de 10% sur le lot de 2 pochettes de 500g (prix d&eacute;j&agrave; remis&eacute;).</strong></p>\r\n</section>', '2018-12-16', 15, 20, 'product3_big.jpg', 1),
+(3, 'Thé bleu', 'Super thé tout bleu', '<p>Youpi !</p>', '2018-12-16', 10, 20, 'product9_big.jpg', 3);
 
 -- --------------------------------------------------------
 
@@ -132,7 +159,18 @@ CREATE TABLE IF NOT EXISTS `productvariation` (
   `product_prod_id` int(10) UNSIGNED NOT NULL,
   PRIMARY KEY (`prodv_id`),
   KEY `fk_productVariation_product1_idx` (`product_prod_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `productvariation`
+--
+
+INSERT INTO `productvariation` (`prodv_id`, `prodv_name`, `prodv_price`, `prodv_quantity`, `product_prod_id`) VALUES
+(1, 'Sachet de 100g', 0, 200, 1),
+(2, 'Sachet de 200g', 15, 150, 1),
+(4, 'Sachet de 300g', 20, 100, 1),
+(5, 'Sachet de 100g', 0, 300, 3),
+(6, 'Sachet de 50g', -5, 100, 1);
 
 --
 -- Contraintes pour les tables déchargées
